9eb3269e48ba58fe12c09561cd15d0d821b97814
Julio <julionietto@gmail.com>
2022-01-07 17:50:22 -0300

    Atualizando

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,9 @@ RSpec.configure do |config|
   include Contato
   include Criar
 
+  config.color = true
+  config.formatter = :documentation
+  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
